2460c97f047e371b898a46feeac8e5269706af07
arpitktyr <arpitktyr@gmail.com>
2023-05-12 00:32:53 +0530

    task planner

--- jsBasic/ConstructorswithPrototypes.js
+++ jsBasic/ConstructorswithPrototypes.js
@@ -16,6 +16,8 @@ console.log(dude.fullName());
 
 console.log(dude.fullName_prototype());
 
+//----------------------------------------------------------------//
+
 //A single instance of fullName_prototype() will now be shared between all person objects.
 //prototype is good for memory consumption;
 //because instances for fullName() method will depend on
@@ -25,11 +27,9 @@ function Man(firstName, lastName) {
   this.firstName = firstName;
   this.lastName = lastName;
 }
-
 Man.prototype.fullName = function () {
   return this.firstName + " " + this.lastName;
 };
-
 function Professional(honorific, firstName, lastName) {
   Man.call(this, firstName, lastName);
   this.honorific = honorific;
@@ -43,3 +43,17 @@ Professional.prototype.professional_name = function () {
 var prof = new Professional("Er", "Aman", "Ktyr");
 console.log(prof.professional_name());
 console.log(prof.fullName()); //Professional.prototype = Object.create(Man.prototype);
+
+//----------------------------------------------------------------
+let car = {
+  modelName: function () {
+    return this.carName + " " + this.year;
+  },
+};
+
+let fordCar = Object.create(car, {
+  carName: { value: "Ford_model" },
+  year: { value: 2022 },
+});
+
+console.log(fordCar.modelName());
